ffe84308b3df837049967dd92044239e21efe478
Sharath Naik <117922641+sharathkumar7777@users.noreply.github.com>
2025-12-26 12:45:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+ const textInput = document.getElementById("text");
+    const delayInput = document.getElementById("delay");
+    const button = document.getElementById("btn");
+    const output = document.getElementById("output");
+
+    function wait(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function showMessage() {
+      const message = textInput.value;
+      const delay = Number(delayInput.value);
+
+      output.textContent = ""; // ensure empty before delay
+
+      await wait(delay);
+
+      output.textContent = message;
+    }
+
+    button.onclick = showMessage;
